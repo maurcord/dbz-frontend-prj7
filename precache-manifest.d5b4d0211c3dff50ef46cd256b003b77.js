37971dd0240d0a8c2c7f2a57ce6d38677388435e
maurcord <67590886+maurcord@users.noreply.github.com>
2020-09-15 16:54:37 -0400

    Updates

---
+++ precache-manifest.d5b4d0211c3dff50ef46cd256b003b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3de1b6b56b0562f067491759b771c3",
+    "url": "/maurcord/Project-7/index.html"
+  },
+  {
+    "revision": "d92c4e49dab0180eb31e",
+    "url": "/maurcord/Project-7/static/css/main.3682b041.chunk.css"
+  },
+  {
+    "revision": "c4f7d9eb0b4bacf00487",
+    "url": "/maurcord/Project-7/static/js/2.7ad0f09b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maurcord/Project-7/static/js/2.7ad0f09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92c4e49dab0180eb31e",
+    "url": "/maurcord/Project-7/static/js/main.1a5118a7.chunk.js"
+  },
+  {
+    "revision": "cdb850e2dd27555c14e6",
+    "url": "/maurcord/Project-7/static/js/runtime-main.b73ac463.js"
+  }
+]);
